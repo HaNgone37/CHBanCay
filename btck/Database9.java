3b5c928448a422122ca64b46ed05b4a61980f10b
HaNgone37 <167754995+HaNgone37@users.noreply.github.com>
2024-05-09 20:31:43 +0700

    Add files via upload

---
+++ btck/Database9.java
@@ -0,0 +1,7 @@
+package btck;
+
+public class Database9 {
+	public static final String url = "jdbc:sqlserver://localhost:1433;databasename=CuaHang_Bancay";
+	public static final String username = "Administrator";
+	public static final String password = "0307";
+}
